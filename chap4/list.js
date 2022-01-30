d22d94fe6785c86e51a6f9e4735f0b0bb89ec919
Kalvin Yu <33174093+kalvinyuu@users.noreply.github.com>
2022-01-30 22:52:43 +0000

    my attempt at ex 2.3 is still challenging.

--- chap4/list.js
+++ chap4/list.js
@@ -1,10 +1,23 @@
 let list = {}
-let rest = {}
+let best = []
+let N  = 0
+
+let jist = { value: 1, rest: { value: 2, rest: { value: 3, rest: {} } } }
 function arrayToList(array) {
     for(let i = array.length - 1; i >= 0; i--) {
 	list = {value: array[i], rest: list};
     }
-    return list
+    return list;
 }
 
+function listToArray() {
+    for (let node = list; node; node = node.rest) {
+	best.push(list.value);
+    }
+    return best;
+}
+function Nth(N) {
+    for (let node = list; node; node = node.rest) {}
+}
 console.log(arrayToList([1, 2, 3]))
+console.log(listToArray(list))
