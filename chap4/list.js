e2b189c85e18938fa8479cd61feb4ff329985217
Kalvin Yu <33174093+kalvinyuu@users.noreply.github.com>
2022-01-31 18:55:33 +0000

    working! i was calling to push the list value instead of node.

--- chap4/list.js
+++ chap4/list.js
@@ -1,23 +1,20 @@
-let list = {}
-let best = []
-let N  = 0
-
-let jist = { value: 1, rest: { value: 2, rest: { value: 3, rest: {} } } }
 function arrayToList(array) {
+    let list = null;
     for(let i = array.length - 1; i >= 0; i--) {
 	list = {value: array[i], rest: list};
     }
     return list;
 }
 
-function listToArray() {
+function listToArray(list) {
+    let array = []
     for (let node = list; node; node = node.rest) {
-	best.push(list.value);
+	array.push(node.value);
     }
-    return best;
+    return array;
 }
 function Nth(N) {
     for (let node = list; node; node = node.rest) {}
 }
 console.log(arrayToList([1, 2, 3]))
-console.log(listToArray(list))
+console.log(listToArray(arrayToList([1, 2, 3])))
